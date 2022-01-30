cf330a2264a08c35ebf0e6a62d594508a5c8f3b8
ZhouYu2003 <2359292349@qq.com>
2022-01-30 19:03:17 +0800

    haydn: Preliminary compatible with crdroid

--- lineage_haydn.mk
+++ lineage_haydn.mk
@@ -21,15 +21,15 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from haydn device
 $(call inherit-product, device/xiaomi/haydn/device.mk)
 
-# Inherit some common PixelExperience stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_SUPPORTS_QUICK_TAP := true
 
 # Boot animation
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier
-PRODUCT_NAME := aosp_haydn
+PRODUCT_NAME := lineage_haydn
 PRODUCT_DEVICE := haydn
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi K40 Pro
